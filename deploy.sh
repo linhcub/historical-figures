09027b469a8398e74f183fed1bf7cabba5fc62ae
Linh Le <levanlinhcube@gmail.com>
2025-11-25 12:19:18 +0700

    add feudal data

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 ssh ubuntu@140.245.117.24 "cd historical-figures && git pull && sudo docker-compose build && sudo docker-compose down && sudo docker-compose up -d"
 ssh ubuntu@140.245.117.24 "sudo docker exec historical_figures_api python scripts/import_csv_to_db.py historical_figures_modern_era.csv"
+ssh ubuntu@140.245.117.24 "sudo docker exec historical_figures_api python scripts/import_csv_to_db.py historical_figures_feudal_era.csv"
